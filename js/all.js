780427bc0ca3d90030aaf488298035f31326fe6a
陳建佑 <97875966+ChienYuYu@users.noreply.github.com>
2022-05-31 11:43:06 +0800

    logout

--- js/all.js
+++ js/all.js
@@ -22,6 +22,8 @@ const goLoginBtn = document.querySelector('#go-login-btn');
 //主頁DOM-----------------------------
 const showUserName = document.querySelector('#show-user-name');
 const logoutBtn = document.querySelector('#logout-btn');
+const inputTodo = document.querySelector('.input-todo');
+const addTodoBtn = document.querySelector('.add-todo-btn');
 //------------------------------------
 
 //登入頁---------------------------
@@ -105,6 +107,7 @@ function signUp(e, n, p) {
       alert(error.response.data.error);
     });
 }
+
 //主頁面---------------------------
 function getTodo() {
   axios.get('https://todoo.5xcamp.us/todos', {
@@ -114,7 +117,7 @@ function getTodo() {
   })
     .then(function (response) {
       console.log(response);
-      if(response.data.todos=[]){
+      if(response.data.todos.length === 0){
         empty.classList.remove('d-none')
         todoList.classList.add('d-none')
       }else{
@@ -126,5 +129,51 @@ function getTodo() {
       console.log(error.response);
     })
 }
-//登出按鈕(待補)
-//---------------------------------
+//登出-------------
+logoutBtn.addEventListener('click',(e)=>{
+  e.preventDefault();
+  logout();
+})
+//詢問問題:為什麼第二個參數不能寫token? 要用物件?
+function logout(){
+  axios.delete('https://todoo.5xcamp.us/users/sign_out',{
+    "headers": {
+      "authorization": token
+    }
+  })
+  .then((response)=>{
+    console.log(response)
+    alert(response.data.message)
+    mainPage.classList.add('d-none')
+    loginSignUp.classList.remove('d-none')
+  })
+  .catch((error)=>console.log(error.response))
+}
+//新增---------------------
+addTodoBtn.addEventListener('click',()=>{
+  let todoItem = inputTodo.value;
+  addTodo(todoItem)
+})
+
+function addTodo(item){
+  axios.post('https://todoo.5xcamp.us/todos',{
+    "todo": {
+      "content": item
+    }
+  },{
+    "headers": {
+      "authorization": token
+    }
+  })
+  .then((response)=>{
+    console.log(response);
+    // getTodo()
+  })
+  .catch((error)=>console.log(error.response))
+}
+
+// Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjg0Iiwic2NwIjoidXNlciIsImF1ZCI6bnVsbCwiaWF0IjoxNjUzOTY2NDU5LCJleHAiOjE2NTUyNjI0NTksImp0aSI6IjVkMWI1Y2YwLTAxOTgtNDE5OS05NDlhLTJiMDJlMDA4NGZiYSJ9.Sa1ACu_00C6Wa-_kLsvx22S-wLNvfEC4T-Pv_jIfNa0
+
+// Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjg0Iiwic2NwIjoidXNlciIsImF1ZCI6bnVsbCwiaWF0IjoxNjUzOTY2NjY5LCJleHAiOjE2NTUyNjI2NjksImp0aSI6ImE3Y2JhNmI4LTVkN2UtNGM4MS05ZmQyLWM2Njc1ZmRiNzYyMyJ9.Rjzj-eBHWE2xjMoBuEdY4uCcYX5k4I1NCZoGhlm4S-M
+
+// Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjg0Iiwic2NwIjoidXNlciIsImF1ZCI6bnVsbCwiaWF0IjoxNjUzOTY2NzYxLCJleHAiOjE2NTUyNjI3NjEsImp0aSI6IjY0OGYxMzM4LTg2YzYtNDZiMy05NWRjLTJmNDFkMTA4NjUwYSJ9.IgWdzmzOxBIZ06Sx1LIfIfUjWwMWA3vBQ9wgJytwOfg
