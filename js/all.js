9b9e77ebbc24ef4e419a2ba1eaefe51397a43db3
陳建佑 <97875966+ChienYuYu@users.noreply.github.com>
2022-06-01 13:01:20 +0800

    edit

--- js/all.js
+++ js/all.js
@@ -1,5 +1,5 @@
 
-const loginSignUp =document.querySelector('#login-signup')
+const loginSignUp = document.querySelector('#login-signup')
 const loginPage = document.querySelector('#login-page');
 const signUpPage = document.querySelector('#signup-page');
 const mainPage = document.querySelector('#main-page');
@@ -7,7 +7,7 @@ const todoList = document.querySelector('.todolist');
 const empty = document.querySelector('.empty');
 // let token = "";
 let todoData = [];//抓出陣列資料放這裡
-const apiUrl ="https://todoo.5xcamp.us";
+const apiUrl = "https://todoo.5xcamp.us";
 //登入頁DOM---------------------------
 const loginEmail = document.querySelector('#login-email');
 const loginPassword = document.querySelector('#login-password');
@@ -27,7 +27,10 @@ const logoutBtn = document.querySelector('#logout-btn');
 const inputTodo = document.querySelector('.input-todo');
 const addTodoBtn = document.querySelector('.add-todo-btn');
 const list = document.querySelector('.list');
-const deleteBtn = document.querySelectorAll('.delete-btn');
+// const deleteBtn = document.querySelectorAll('.delete-btn');
+// const editBtn = document.querySelectorAll('edit-btn');
+const editArea = document.querySelector('.edit-area');
+const newTodo = document.querySelector('.new-todo')
 //------------------------------------
 
 //登入頁----
@@ -120,10 +123,10 @@ function getTodo() {
     .then(function (response) {
       console.log(response);
       // 有待辦無待辦顯示不同畫面---
-      if(response.data.todos.length === 0){
+      if (response.data.todos.length === 0) {
         empty.classList.remove('d-none')
         todoList.classList.add('d-none')
-      }else{
+      } else {
         todoList.classList.remove('d-none')
         empty.classList.add('d-none')
       }
@@ -136,72 +139,108 @@ function getTodo() {
     })
 }
 //渲染資料 renderData()---
-function renderData(){
-  let str=""
-  todoData.forEach(function(item,index){
-  str +=`<li>
+function renderData() {
+  let str = ""
+  todoData.forEach(function (item, index) {
+    str += `<li>
     <label class="d-flex align-items-center border-bottom">
       <input type="checkbox" class="m-3">
       <span>${item.content}</span>
-      <a href="#" class="bi bi-pencil text-decoration-none text-dark p-3 ms-auto" data-id="${item.id}"></a>
+      <a href="#" class="bi bi-pencil text-decoration-none text-dark p-3 ms-auto" data-id="${item.id}" data-element="edit-btn"></a>
       <a href="#" class="bi bi-x-lg text-decoration-none text-dark p-3" data-id="${item.id}" data-element="delete-btn"></a>
     </label>
   </li>`
-  
+
   })
   list.innerHTML = str
 }
 
 //登出-------------
-logoutBtn.addEventListener('click',(e)=>{
+logoutBtn.addEventListener('click', (e) => {
   e.preventDefault();
   logout();
 })
 //logout()
-function logout(){
+function logout() {
   axios.delete(`${apiUrl}/users/sign_out`)
-  .then((response)=>{
-    console.log(response)
-    alert(response.data.message)
-    mainPage.classList.add('d-none')
-    loginSignUp.classList.remove('d-none')
-  })
-  .catch((error)=>console.log(error.response))
+    .then((response) => {
+      console.log(response)
+      alert(response.data.message)
+      mainPage.classList.add('d-none')
+      loginSignUp.classList.remove('d-none')
+    })
+    .catch((error) => console.log(error.response))
 }
 //新增-----
-addTodoBtn.addEventListener('click',()=>{
+addTodoBtn.addEventListener('click', () => {
   let todoItem = inputTodo.value
   addTodo(todoItem)
   inputTodo.value = ''
 })
 //鍵盤Enter新增------(待做)
-function addTodo(item){
-  axios.post(`${apiUrl}/todos`,{
+function addTodo(item) {
+  axios.post(`${apiUrl}/todos`, {
     "todo": {
       "content": item
     }
   })
-  .then((response)=>{
-    console.log(response);
-    getTodo()
-  })
-  .catch((error)=>console.log(error.response))
+    .then((response) => {
+      console.log(response);
+      getTodo()
+    })
+    .catch((error) => console.log(error.response))
 }
 
-//刪除------
-list.addEventListener('click',function(e){
+//刪除---編輯---
+list.addEventListener('click', function (e) {
   let id = e.target.getAttribute('data-id')
-  if(e.target.getAttribute('data-element') == 'delete-btn'){
+  //叉叉
+  if (e.target.getAttribute('data-element') == 'delete-btn') {
     e.preventDefault()
     deleteTodo(id)
   }
+  //鉛筆
+  if (e.target.getAttribute('data-element') == 'edit-btn') {
+    e.preventDefault()
+    editArea.classList.remove('d-none')//顯示編輯視窗
+    editArea.addEventListener('click', (e) => {
+      let ntValue = newTodo.value
+      if ((e.target.getAttribute('data-element') === 'confirm-btn') &&
+        (ntValue.trim() !== "")) {
+        e.preventDefault()
+        editTodo(id, ntValue)
+        newTodo.value = ""//清除欄位文字
+        editArea.classList.add('d-none')
+      }  else if (e.target.getAttribute('data-element') === 'cancel-btn') {
+        e.preventDefault()
+        newTodo.value = ""//清除欄位文字
+        editArea.classList.add('d-none')
+      } else { return }
+    })
+  }
 })
 
-function deleteTodo(id){
+//deleteTodo()
+function deleteTodo(id) {
   axios.delete(`${apiUrl}/todos/${id}`)
-  .then((res)=>{
-    console.log(res)
-    getTodo()
-  })
-  .catch(error=>console.log(error.response))
+    .then((res) => {
+      console.log(res)
+      getTodo()
+    })
+    .catch(error => console.log(error.response))
 }
+//editTodo()
+function editTodo(id, ntValue) {
+  axios.put(`${apiUrl}/todos/${id}`, {
+    "todo": {
+      "content": ntValue
+    }
+  })
+    .then(res => {
+      console.log(res)
+      getTodo()
+    })
+    .catch(error => console.log(error.response))
+}
+
+//------------
